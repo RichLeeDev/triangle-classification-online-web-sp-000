58423025371d3b3052589b8f937f35c1fc4ccecc
Richard Lee <richlee81192@gmail.com>
2019-10-24 17:05:30 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,25 @@
 class Triangle
-  # write code here
+  attr_reader :a, :b, :c
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
+  end
+
+  def kind
+    validate_triangle #runs this method
+    #returns what kind of triangle it is
+    if a == b && b == c
+      :equilateral
+    elsif a == b || b == c || a == c
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+
+end
+
+class TriangleError < StandardError
 end
